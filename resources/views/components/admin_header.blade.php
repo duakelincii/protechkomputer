aaaa0b9a82b35f67550da26e3dbf7a5c1a54e2b4
Kenny Widodo <widodokenny@gmail.com>
2020-12-04 13:53:39 +0700

    changed the admin profile picture

--- resources/views/components/admin_header.blade.php
+++ resources/views/components/admin_header.blade.php
@@ -24,14 +24,14 @@
                     <!-- Menu Toggle Button -->
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <!-- The user image in the navbar-->
-                        <img src="{{asset('')}}assets/admin/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                        <img src="{{asset('')}}assets/main/img/avatar.png" class="user-image" alt="User Image">
                         <!-- hidden-xs hides the username on small devices so only the image appears. -->
                         <span class="hidden-xs">{{Session::get('admin')['admin_name']}}</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- The user image in the menu -->
                         <li class="user-header">
-                        <img src="{{asset('')}}assets/admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="{{asset('')}}assets/main/img/avatar.png" class="img-circle" alt="User Image">
         
                         <p>
                             {{Session::get('admin')['admin_name']}}
